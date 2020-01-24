7c7e41b255179a6d38e05d60f10d678386ec633f
jesusproint998 <jesusproint998@gmail.com>
2020-01-24 14:06:21 +0100

    Cambio nombre atributo correo a email

--- p52/p52Jesus/src/main/java/daw/p52jesus/Cliente.java
+++ p52/p52Jesus/src/main/java/daw/p52jesus/Cliente.java
@@ -5,13 +5,51 @@ package daw.p52jesus;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author usuario
  */
 public class Cliente {
+
     private String nombre;
     private int edad;
-    private String correo;
+    private String email;
+
+    public Cliente(String nombre, int edad, String email) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", edad=" + edad + ", email=" + email + '}';
+    }
+
+
+    
 }
