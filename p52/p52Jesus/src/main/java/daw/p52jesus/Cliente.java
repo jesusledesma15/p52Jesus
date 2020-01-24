c607410b8dc0ed3979ad47ee20ce96577624a274
jesusproint998 <jesusproint998@gmail.com>
2020-01-24 13:57:04 +0100

    Añade cliente

---
+++ p52/p52Jesus/src/main/java/daw/p52jesus/Cliente.java
@@ -0,0 +1,17 @@
+package daw.p52jesus;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class Cliente {
+    private String nombre;
+    private int edad;
+    private String correo;
+}
